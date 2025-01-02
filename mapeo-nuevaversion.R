9d6d3f9bf8fd32a6b22b565d3d0dd830b24a6df2
jatziri2 <jatzirilin@ciencias.unam.mx>
2025-01-02 01:03:07 -0600

    Mayor consistencia de la filogenia

--- mapeo-nuevaversion.R
+++ mapeo-nuevaversion.R
@@ -42,35 +42,45 @@ if (any(is.na(tip_colors))) {
 # Inicializa colores de las ramas
 edge_colors <- rep("gray70", nrow(ptero_cropped$edge))
 
-# Función recursiva corregida (con phytools::getDescendants())
+# Función recursiva corregida para colorear las ramas internas
 assign_edge_colors <- function(tree, tip_colors, edge_colors) {
   n_tips <- Ntip(tree)
-  for (node in (n_tips + Nnode(tree)):(n_tips + 1)) {
-    desc <- phytools::getDescendants(tree, node)
-    if (!is.null(desc)) {
-      desc_colors <- c()
-      for(d in desc){
-        if(d <= n_tips){
-          desc_colors <- c(desc_colors, tip_colors[d])
-        } else {
-          edge_index <- which(tree$edge[,2] == d)
-          desc_colors <- c(desc_colors, edge_colors[edge_index])
-        }
+  
+  # Orden de nodos: de las hojas a la raíz
+  node_order <- rev((n_tips + 1):(n_tips + Nnode(tree)))
+  
+  for (node in node_order) {
+    # Obtiene los descendientes inmediatos (hojas o nodos internos)
+    desc_edges <- which(tree$edge[, 1] == node)
+    desc_nodes <- tree$edge[desc_edges, 2]
+    
+    # Colores de los descendientes
+    desc_colors <- sapply(desc_nodes, function(desc) {
+      if (desc <= n_tips) {
+        return(tip_colors[desc])
+      } else {
+        edge_index <- which(tree$edge[, 2] == desc)
+        return(edge_colors[edge_index])
       }
-      predominant_color <- names(sort(table(desc_colors), decreasing = TRUE))[1]
-      edge_index <- which(tree$edge[, 2] == node)
-      edge_colors[edge_index] <- predominant_color
-    }
+    })
+    
+    # Color predominante entre los descendientes
+    predominant_color <- names(sort(table(desc_colors), decreasing = TRUE))[1]
+    
+    # Asigna el color predominante a la rama que conecta con este nodo
+    edge_index <- which(tree$edge[, 2] == node)
+    edge_colors[edge_index] <- predominant_color
   }
+  
   return(edge_colors)
 }
 
-# Aplica la función
+# Aplica la función para asignar colores a las ramas internas
 edge_colors <- assign_edge_colors(ptero_cropped, tip_colors, edge_colors)
 
 # Colorea las ramas terminales
-for(i in 1:length(ptero_cropped$tip.label)){
-  edge_index <- which(ptero_cropped$edge[,2] == i)
+for (i in 1:length(ptero_cropped$tip.label)) {
+  edge_index <- which(ptero_cropped$edge[, 2] == i)
   edge_colors[edge_index] <- tip_colors[i]
 }
 
@@ -82,7 +92,7 @@ plot(ptero_cropped,
      label.offset = 0.005,
      edge.width = 2)
 
-# Leyenda (modificada para incluir todos los valores)
+# Leyenda para los colores
 legend_labels <- c(names(original_colors), names(special_colors))
 legend_colors <- c(original_colors, special_colors)
 legend(x = "bottomleft", legend = legend_labels, pch = 22,
